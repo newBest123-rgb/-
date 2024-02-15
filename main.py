a893164dd482eb4075bc4ad5f116ee2bf43c03e9
Eger_tech <133259370+newBest123-rgb@users.noreply.github.com>
2024-02-15 20:22:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,11 @@ def promotion():
         return des.read()
 
 
+@app.route('/image_mars')
+def image_mars():
+    with open('image.html', 'r', encoding='utf-8') as image:
+        return image.read()
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1', debug=True)
-ыпваывапы
